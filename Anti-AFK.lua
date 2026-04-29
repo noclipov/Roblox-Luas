a14b347701600016677e55b6c6b2e63f00937e98
noclipov <91428127+noclipov@users.noreply.github.com>
2026-04-29 14:02:26 +0300

    1

--- Anti-AFK.lua
+++ Anti-AFK.lua
@@ -1,7 +1,6 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/dimanoclip/Roblox-Luas/main/Anti-AFK.lua"))()
-if not _G.Useless or not _G.Useless.AntiAfkLoaded then
-	_G.Useless = {}
-	_G.Useless.AntiAfkLoaded = false
+if not _G.AntiAfkLoaded then
+	_G.AntiAfkLoaded = false
 	local gcn = getconnections or get_signal_cons
 	if gcn then
 		for i, v in gcn(game.Players.LocalPlayer.Idled) do
@@ -20,6 +19,6 @@ if not _G.Useless or not _G.Useless.AntiAfkLoaded then
 		VirtualUser:ClickButton2(Vector2.new())
 	end)
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/dimanoclip/Roblox-Luas/main/Libs/NotifyModule.lua"))().Mini("Success", "Anti-AFK Loaded", 2)
-	_G.Useless.AntiAfkLoaded = true
-	queue_on_teleport('_G.Useless.AntiAfkLoaded = false; loadstring(game:HttpGet("https://raw.githubusercontent.com/dimanoclip/Roblox-Luas/main/Anti-AFK.lua"))()')
+	_G.AntiAfkLoaded = true
+	queue_on_teleport('_G.AntiAfkLoaded = false; task.wait(0.2); loadstring(game:HttpGet("https://raw.githubusercontent.com/dimanoclip/Roblox-Luas/main/Anti-AFK.lua"))()')
 end
