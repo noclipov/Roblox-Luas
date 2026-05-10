516da8b56c8558ec669bdfb4b37b26ad68e72b02
dimanoclip <gamefolover2019@gmail.com>
2026-05-10 11:06:02 +0300

    1

---
+++ Libs/dex/header.lua
@@ -0,0 +1,47 @@
+--[[
+	Dex++
+	Version 3.0
+	
+	Developed by Chillz
+	
+	Dex++ is a revival of Moon's Dex, made to fulfill Moon's Dex prophecy.
+]]
+
+local selection
+local nodes = {}
+
+local oldgame = game
+local game = workspace.Parent
+
+cloneref = cloneref or function(ref)
+	if not getreg then return ref end
+	
+	local InstanceList
+	
+	local a = Instance.new("Part")
+	for _, c in pairs(getreg()) do
+		if type(c) == "table" and #c then
+			if rawget(c, "__mode") == "kvs" then
+				for d, e in pairs(c) do
+					if e == a then
+						InstanceList = c
+						break
+					end
+				end
+			end
+		end
+	end
+	local f = {}
+	function f.invalidate(g)
+		if not InstanceList then
+			return
+		end
+		for b, c in pairs(InstanceList) do
+			if c == g then
+				InstanceList[b] = nil
+				return g
+			end
+		end
+	end
+	return f.invalidate
+end
