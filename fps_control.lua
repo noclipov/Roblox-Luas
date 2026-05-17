f8bb9c794321017ea71d1955a2ab544f885d1758
dimanoclip <gamefolover2019@gmail.com>
2026-05-17 07:04:15 +0300

    1

--- fps_control.lua
+++ fps_control.lua
@@ -1,5 +1,6 @@
 if not isrbxactive or not setfpscap then return end
+local maxfps = getfpscap and getfpscap() or 120
 game:GetService("RunService").RenderStepped:Connect(function()
-	if isrbxactive and not isrbxactive() then setfpscap(15)
+	if not isrbxactive() then setfpscap(15)
 	else setfpscap(120) end
 end)
