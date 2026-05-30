acb2318c530c6c63b780c67f2c7bb22dcb739b07
dimanoclip <gamefolover2019@gmail.com>
2026-05-30 19:43:25 +0300

    1

--- anti_afk.lua
+++ anti_afk.lua
@@ -1,4 +1,4 @@
-﻿-- loadstring(game:HttpGet("https://raw.githubusercontent.com/noclipov/Roblox-Luas/main/anti_afk.lua"))()
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/noclipov/Roblox-Luas/main/anti_afk.lua"))()
 while not game.IsLoaded do task.wait() end
 local LINK = "https://raw.githubusercontent.com/noclipov/Roblox-Luas/main/anti_afk.lua"
 local msg = loadstring(game:HttpGet("https://raw.githubusercontent.com/noclipov/Roblox-Luas/main/Libs/notify.lua"))()
